e854cd606252a6701d754969419b0dbfe5ad63a2
Matthew Pearce <matthewpearce@Matthews-MacBook-Pro.local>
2020-06-07 11:28:09 +0200

    Flat list producing correct items

--- src/Tracker.js
+++ src/Tracker.js
@@ -9,9 +9,9 @@ import {
 } from "react-native";
 
 import Card from "../components/card";
-// edamam keys
-// const APP_ID = "9ef9baef";
-// const APP_KEY = "f48b3d6c5374f60449cfe909f947a540";
+
+const APP_ID = "9ef9baef";
+const APP_KEY = "f48b3d6c5374f60449cfe909f947a540";
 
 /**
  * Profile screen
@@ -34,72 +34,15 @@ export default class Tracker extends React.Component {
   fetchData = (item) => {
     console.log(item);
     fetch(
-      // edamam `https://api.edamam.com/api/food-database/parser?ingr=${item}&app_id=${APP_ID}&app_key=${APP_KEY}`
-      `https://trackapi.nutritionix.com/v2/search/instant?query=${item}`,
-      {
-        headers: {
-          "x-app-id": "0f092d40",
-          "x-app-key": "a9d3743339dce2115fb110ecc7ce7214",
-        },
-      }
+      `https://api.edamam.com/api/food-database/parser?ingr=${item}&app_id=${APP_ID}&app_key=${APP_KEY}`
     )
       .then((response) => response.json())
       .then((responseJson) => {
         console.log(responseJson);
-        console.log(responseJson.branded[0].food_name);
-        // alert(JSON.stringify(responseJson)); //ingr this is your key this is wat the api will return for you. so we need to send whichver food item that we are looking for / can we let a user search and return an item? yes. heymaybe i can call you an explain to you yes on what?watspp? i do not have discord?..... wait teamviewer has call featureok    // can you show me how to search for an item? yes sure
+
+        //  alert(JSON.stringify(responseJson)); //ingr this is your key this is wat the api will return for you. so we need to send whichver food item that we are looking for / can we let a user search and return an item? yes. heymaybe i can call you an explain to you yes on what?watspp? i do not have discord?..... wait teamviewer has call featureok    // can you show me how to search for an item? yes sure
         this.setState({
-          itemArray: responseJson.parsed,
-          branded: responseJson.branded,
-          isLoading: false,
-          dataSource: null,
-        });
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-
-  //second method to get nutrients
-  fetchData = (item) => {
-    console.log(item);
-    fetch(
-      // edamam `https://api.edamam.com/api/food-database/parser?ingr=${item}&app_id=${APP_ID}&app_key=${APP_KEY}`
-      `https://trackapi.nutritionix.com/v2/search/instant?query=${item}`,
-      {
-        headers: {
-          "x-app-id": "0f092d40",
-          "x-app-key": "a9d3743339dce2115fb110ecc7ce7214",
-        },
-      }
-    )
-      .then((response) => response.json())
-      .then((responseJson) => {
-        console.log(responseJson);
-        console.log(responseJson.branded[0].food_name);
-        console.log(responseJson.branded[0].nix_item_id);
-        // alert(JSON.stringify(responseJson));
-     /* 
-        let x = 
-        fetch(
-             `https://trackapi.nutritionix.com/v2/search/item?nix_item_id=${x}`,
-
-            headers: {
-              "x-app-id": "0f092d40",
-              "x-app-key": "a9d3743339dce2115fb110ecc7ce7214",
-            },
-          }
-          .then((res) => {})
-          .then((anotherJson) -> {})
-        );
-        
-         */ 
-
-        this.setState({
-          itemArray: responseJson.parsed,
-          branded: responseJson.branded,
-          isLoading: false,
-          dataSource: null,
+          itemArray: responseJson.hints,
         });
       })
       .catch((error) => {
@@ -109,7 +52,6 @@ export default class Tracker extends React.Component {
 
   render() {
     const { navigate, state } = this.props.navigation;
-
     return (
       //styling for navigation container
       <View style={styles.container}>
@@ -124,17 +66,18 @@ export default class Tracker extends React.Component {
             title="Search"
             onPress={() => this.fetchData(this.state.item)}
           />
-
           <FlatList
             style={styles.resultsBackground}
-            data={this.state.branded}
-            renderItem={({ item, index }) => (
-             <View
-             style={styles.outViewStyle}>
-             <View style={styles.resultsStyle}>
-                <Text style={styles.resultsText}>{item.food_name}</Text>
-                <Text> F: </Text>
-              </View>
+            data={this.state.itemArray}
+            renderItem={({ item }) => (
+              <View style={styles.outViewStyle}>
+                <View style={styles.resultsStyle}>
+                  <Text style={styles.resultsText}>
+                    {item.food.label}
+                    {item.food.brand}
+                  </Text>
+                  <Text> F: {item.food.nutrients.FAT}</Text>
+                </View>
               </View>
             )}
           />
@@ -185,7 +128,7 @@ const styles = StyleSheet.create({
   },
   textInputContainer: {
     borderColor: "black",
-    borderWidth: 1,
+    borderBottomWidth: 1,
     width: "70%",
     color: "#919191",
     fontSize: 22,
@@ -208,7 +151,6 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     backgroundColor: "#5979D9",
     shadowOpacity: 0.3,
-    backfaceVisibility: "hidden"
   },
 
   resultsText: {
@@ -216,6 +158,6 @@ const styles = StyleSheet.create({
     color: "black",
   },
   outViewStyle: {
-    padding : 10
-  }
+    padding: 10,
+  },
 });
